02f3cbd62baef7aa077ea4466f336bf254f17ed4
Francisco Vaz <ftomasvaz@gmail.com>
2020-09-07 22:20:09 +0100

    add immer para trabalhar com os estados imutaveis

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,4 +1,5 @@
 import { Reducer } from "redux";
+import produce from "immer";
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
@@ -7,25 +8,25 @@ const INITIAL_STATE: ICartState = {
 
 // No 1º momento, em vez de vir undefined, o state fica como INITIAL_STATE
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case "ADD_PRODUCT_TO_CART": {
-      const { product } = action.payload;
+  //produce recebe o estado anterior e um rascunho do estado a alterar
+  //No final, o produce compara o rascunho com o state e faz as alteraçōes de forma automatica
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case "ADD_PRODUCT_TO_CART": {
+        const { product } = action.payload;
 
-      return {
-        ...state,
-        items: [
-          ...state.items,
-          {
-            product,
-            quantity: 1,
-          },
-        ],
-      };
+        draft.items.push({
+          product,
+          quantity: 1,
+        });
+
+        break;
+      }
+      default: {
+        return draft;
+      }
     }
-    default: {
-      return state;
-    }
-  }
+  });
 };
 
 export default cart;
