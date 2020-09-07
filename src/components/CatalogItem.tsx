4d22cdb4dcb7e3c2865237d6fb39540726192574
Francisco Vaz <ftomasvaz@gmail.com>
2020-09-07 23:26:21 +0100

    abstract item from catalog

---
+++ src/components/CatalogItem.tsx
@@ -0,0 +1,29 @@
+import React, { useCallback } from "react";
+import { IProduct } from "../store/modules/cart/types";
+import { addProductToCart } from "../store/modules/cart/actions";
+import { useDispatch } from "react-redux";
+
+// import { Container } from './styles';
+
+interface ICatalogItemProps {
+  product: IProduct;
+}
+
+const CatalogItem: React.FC<ICatalogItemProps> = ({ product }) => {
+  const dispatch = useDispatch();
+  const handleAddProductToCart = useCallback(() => {
+    dispatch(addProductToCart(product));
+  }, [dispatch, product]);
+
+  return (
+    <article key={product.id}>
+      <strong>{product.title}</strong> {" - "}
+      <span>{product.price}</span>
+      <button type="button" onClick={handleAddProductToCart}>
+        Comprar
+      </button>
+    </article>
+  );
+};
+
+export default CatalogItem;
